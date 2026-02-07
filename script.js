cb446d86c1e72191d4cf66d19eac070946442597
parimalad9614-cyber <parimala.d9614@gmail.com>
2026-02-07 21:27:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Typing Effect
 const text = "Happy Birthday My Love ❤️";
 let index = 0;
 const typingElement = document.getElementById("typingText");
- 
+
 function typeEffect() {
     if (index < text.length) {
         typingElement.innerHTML += text.charAt(index);
@@ -11,26 +10,20 @@ function typeEffect() {
     }
 }
 typeEffect();
- 
-// Start Surprise
+
 function startSurprise() {
     document.getElementById("bgMusic").play();
-    document.getElementById("story")
-        .scrollIntoView({ behavior: "smooth" });
+    document.getElementById("story").scrollIntoView({ behavior: "smooth" });
 }
- 
-// Navigation
+
 function goToMemories() {
-    document.getElementById("memories")
-        .scrollIntoView({ behavior: "smooth" });
+    document.getElementById("memories").scrollIntoView({ behavior: "smooth" });
 }
- 
+
 function goToLetter() {
-    document.getElementById("letter")
-        .scrollIntoView({ behavior: "smooth" });
+    document.getElementById("letter").scrollIntoView({ behavior: "smooth" });
 }
- 
-// Reveal Letter
+
 function revealLetter() {
-    document.getElementById("loveLetter").style.display = "block";
+    document.getElementById("loveLetter").classList.remove("hidden");
 }
